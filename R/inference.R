379d83763fd4ef6db23cb6a253828e11388ebf66
Peter Schulam <pschulam@gmail.com>
2014-03-06 14:01:15 -0500

    Restrict posterior inference to weights w (variance known).

--- R/inference.R
+++ R/inference.R
@@ -28,25 +28,17 @@ observe.bayeslr <- function(blr, X, y) {
 
     blr <- bayeslr_append_evidence(blr, X, y)
 
-    ## Normal normal-inverse-gamma posterior (see page 235 of Murphy; equations 7.69-7.73)
+    ## Standard Bayesian LM posterior updates (see page 232 of Murphy; equations 7.56-7.58).
 
-    w_cov <- solve(solve(blr$w_cov) + crossprod(X))  # Murphy eq. 7.71
+    s2 <- blr$sigsq
 
-    w_mean <- w_cov %*% (solve(blr$w_cov, blr$w_mean) +  t(X) %*% y)  # Murphy eq. 7.70
+    w_cov <- s2 * solve(s2 * blr$w_cov + crossprod(X))  # Murphy eq. 7.58
 
-    sigsq_a <- blr$sigsq_a + n / 2  # Murphy eq. 7.72
-
-    q1 <- crossprod(w_mean, solve(w_cov, w_mean))
-    q1 <- as.double(q1)
-    q2 <- crossprod(blr$w_mean, solve(blr$w_cov, blr$w_mean))
-    q2 <- as.double(q2)
-    sigsq_b <- blr$sigsq_b + 0.5 * (q1 + as.double(crossprod(y)) + q2)  # Murphy eq. 7.73
+    w_mean <- w_cov %*% solve(blr$w_cov, blr$w_mean) + (1 / s2) * w_cov %*% crossprod(X, y)  # Murpy eq. 7.56
 
     ## Update bayeslr object.
 
     blr$w_mean <- w_mean
     blr$w_cov <- w_cov
-    blr$sigsq_a <- sigsq_a
-    blr$sigsq_b <- sigsq_b
     blr
 }
