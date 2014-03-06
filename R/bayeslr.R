0abcf138b7d57d4e4bfd4c9def5fe60c9baa13c0
Peter Schulam <pschulam@gmail.com>
2014-03-06 15:17:30 -0500

    Add plot method for bayeslr object.

--- R/bayeslr.R
+++ R/bayeslr.R
@@ -1,7 +1,10 @@
 #' Functions for fitting and using Bayesian linear regression models.
 #'
+#' @import ggplot2
+#' @importFrom mvtnorm rmvnorm
 #' @docType package
 #' @name bayeslr
+NULL
 
 #' Create a new 'bayeslr' object
 #'
@@ -41,3 +44,33 @@ dim.bayeslr <- function(blr) {
     length(blr$w_mean)
 }
 
+#' Plot bayeslr object
+#'
+#' @export
+#' 
+plot.bayeslr <- function(blr) {
+    w_mean <- blr$w_mean
+    w_sd <- sqrt(diag(blr$w_cov))
+    
+    density_evals <- vector("list", dim(blr))
+
+    for (ix in seq_along(w_mean)) {
+        wm <- w_mean[ix]
+        ws <- w_sd[ix]
+
+        dfunc <- function(x) dnorm(x, wm, ws)
+        lo <- wm - 5 * ws
+        hi <- wm + 5 * ws
+
+        density_evals[[ix]] <- dense_data_frame(dfunc, lo, hi, 100, param_num = ix)
+    }
+
+    p <- ggplot(do.call(rbind, density_evals))
+    p <- p + geom_line(aes(x, y))
+    p <- p + facet_wrap(~ param_num)
+    p <- p + labs(title = "Coefficient Distributions", x = "w", y = "p(w)")
+    p <- p + theme_bw()
+
+    print(p)
+    invisible(p)
+}
--- R/util.R
+++ R/util.R
@@ -16,3 +16,25 @@ bayeslr_append_evidence <- function(blr, X, y) {
 bayeslr_has_evidence <- function(blr) {
     (!is.null(blr$X)) & (!is.null(blr$y))
 }
+
+#' Evaluate function on dense data frame
+#'
+#' Create data frames of dense function evaluations. Works well with
+#' ggplot2. 'func' argument should be vectorized.
+#'
+#' 
+dense_data_frame <- function(func, from, to, length.out, ...) {
+    x <- seq(from, to, length.out = length.out)
+    y <- func(x)
+    dense_df <- data.frame(x = x, y = y)
+
+    extra_cols <- list(...)
+
+    for (ix in seq_along(extra_cols)) {
+        label <- names(extra_cols)[ix]
+        value <- extra_cols[[ix]]
+        dense_df[[label]] <- value
+    }
+
+    dense_df
+}
