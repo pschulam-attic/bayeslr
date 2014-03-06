ac118a5441a2d6c65685c7267c8145575e493943
Peter Schulam <pschulam@gmail.com>
2014-03-06 10:56:57 -0500

    Add observe function in inference.R.

--- R/inference.R
+++ R/inference.R
@@ -28,10 +28,25 @@ observe.bayeslr <- function(blr, X, y) {
 
     blr <- bayeslr_append_evidence(blr, X, y)
 
-    ## Update the expected value of 'w'.
+    ## Normal normal-inverse-gamma posterior (see page 235 of Murphy; equations 7.69-7.73)
 
-    ## Update the covariance of 'w'.
+    w_cov <- solve(solve(blr$w_cov) + crossprod(X))  # Murphy eq. 7.71
 
-    ## Update inverse gamma parameters for 'sigsq'.
-    
+    w_mean <- w_cov %*% (solve(blr$w_cov, blr$w_mean) +  t(X) %*% y)  # Murphy eq. 7.70
+
+    sigsq_a <- blr$sigsq_a + n / 2  # Murphy eq. 7.72
+
+    q1 <- crossprod(w_mean, solve(w_cov, w_mean))
+    q1 <- as.double(q1)
+    q2 <- crossprod(blr$w_mean, solve(blr$w_cov, blr$w_mean))
+    q2 <- as.double(q2)
+    sigsq_b <- blr$sigsq_b + 0.5 * (q1 + as.double(y %*% y) + q2)  # Murphy eq. 7.73
+
+    ## Update bayeslr object.
+
+    blr$w_mean <- w_mean
+    blr$w_cov <- w_cov
+    blr$sigsq_a <- sigsq_a
+    blr$sigsq_b <- sigsq_b
+    blr
 }
